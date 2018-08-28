64a3638f487d9b282b459a60b56f2f250ba3c9e9
Kirk Scheibelhut <kjs@google.com>
2018-08-28 10:47:40 -0700

    Separate common functionality into a library

--- cmd/efforts/efforts.go
+++ cmd/efforts/efforts.go
@@ -1,91 +1,48 @@
-// > go run brad.go -token=$BRAD_STRAVA_ACCESS_TOKEN -climbs=climbs.json
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"sort"
 	"time"
 
 	"github.com/scheibo/perf"
+	. "github.com/scheibo/stravutils"
 	"github.com/strava/go.strava"
 )
 
-const NUM_BEST = 5
-const WEIGHT_BEST = 1
-const NUM_YEAR = 5
-const WEIGHT_YEAR = 2
-const NUM_RECENT = 5
-const WEIGHT_RECENT = 4
-
-const MAX_PER_PAGE = 200
-
-type Climb struct {
-	Name      string   `json:"name"`
-	Aliases   []string `json:"aliases"`
-	SegmentId int64    `json:"segment_id"`
-}
 
 type Effort struct {
 	climb   Climb
-	segment *strava.SegmentDetailed
 	effort  *strava.SegmentEffortSummary
 	score   float64
 }
 
 func main() {
-	var accessToken string
-	var climbsFile string
+	var token, climbsFile string
 	var climbs []Climb
-
-	var segments []*strava.SegmentDetailed
 	var efforts []*Effort
 
-	// Provide an access token, with write permissions.
-	// You'll need to complete the oauth flow to get one.
-	flag.StringVar(&accessToken, "token", "", "Access Token")
+	flag.StringVar(&token, "token", "", "Access Token")
 	flag.StringVar(&climbsFile, "climbs", "", "Climbs")
 
 	flag.Parse()
 
-	if accessToken == "" {
-		fmt.Println("\nPlease provide an access_token, one can be found at https://www.strava.com/settings/api")
-
-		flag.PrintDefaults()
-		os.Exit(1)
+	climbs, err := GetClimbs(climbsFile)
+	if err != nil {
+		exit(err)
 	}
 
-	if climbsFile == "" {
-		fmt.Println("\nPlease provide a JSON file name pointing towards climbs.")
-
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
-
-	file, err := ioutil.ReadFile(climbsFile)
-	maybeExit(err)
-	err = json.Unmarshal(file, &climbs)
-	maybeExit(err)
-
-	client := strava.NewClient(accessToken)
-	service := strava.NewSegmentsService(client)
 	for _, climb := range climbs {
-		segment, err := service.Get(climb.SegmentId).Do()
-		maybeExit(err)
-		segments = append(segments, segment)
+		es, err := GetEfforts(climb.Segment.ID)
+		if err != nil {
+			exit(err)
+		}
 
-		es, err := service.ListEfforts(climb.SegmentId).
-			PerPage(MAX_PER_PAGE).
-			Do()
-		maybeExit(err)
-
-		h := (segment.ElevationLow + segment.ElevationHigh) / 2
 		for _, e := range es {
-			score := perf.CalcM(float64(e.ElapsedTime), segment.Distance, averageGrade(segment), h)
-			effort := &Effort{climb : climb, segment: segment, effort: e, score: score}
+			score := perf.CalcM(float64(e.ElapsedTime), climb.Segment.Distance, climb.Segment.AverageGrade(), climb.Segment.MedianElevation())
+			effort := &Effort{climb : climb, effort: e, score: score}
 			efforts = append(efforts, effort)
 		}
 	}
@@ -98,15 +55,10 @@ func main() {
 	}
 }
 
-func averageGrade(s *strava.SegmentDetailed) float64 {
-	return (s.ElevationHigh - s.ElevationLow) / s.Distance
-}
-
-func maybeExit(err error) {
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+func exit(err error) {
+	fmt.Fprintf(os.Stderr, "%s\n\n", err)
+	flag.PrintDefaults()
+	os.Exit(1)
 }
 
 type Efforts []*Effort
---
+++ stravutils.go
@@ -0,0 +1,138 @@
+package stravutils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/strava/go.strava"
+)
+
+const MAX_PER_PAGE = 200
+
+type Climb struct {
+	Name    string   `json:"name"`
+	Aliases []string `json:"aliases,omitempty"`
+	Segment Segment  `json:"segment"`
+}
+
+type Segment struct {
+	Name          string  `json:"name"`
+	ID            int64   `json:"id"`
+	Distance      float64 `json:"distance"`
+	ElevationLow  float64 `json:"elevation_low"`
+	ElevationHigh float64 `json:"elevation_high"`
+}
+
+func (s *Segment) TotalElevationGain() float64 {
+	return s.ElevationHigh - s.ElevationLow
+}
+
+func (s *Segment) MedianElevation() float64 {
+	return (s.ElevationLow + s.ElevationHigh) / 2.0
+}
+
+func (s *Segment) AverageGrade() float64 {
+	return s.TotalElevationGain() / s.Distance * 100.0
+}
+
+func GetClimbs(files ...string) ([]Climb, error) {
+	var climbs []Climb
+
+	file := resource("climbs.json")
+	if len(files) > 0 && files[0] != "" {
+		file = files[0]
+	}
+
+	f, err := ioutil.ReadFile(file)
+	if err != nil {
+		return climbs, err
+	}
+
+	err = json.Unmarshal(file, &climbs)
+	if err != nil {
+		return climbs, err
+	}
+
+	return climbs, nil
+}
+
+func GetSegmentById(segmentID int64, climbs []Climbs, tokens ...string) (*Segment, error) {
+	for _, c := range climbs {
+		if c.Segment.ID == segmentID {
+			return &c.Segment, nil
+		}
+	}
+
+	token, err := getToken(tokens...)
+	if err != nil {
+		return nil, err
+	}
+
+	service, err := getSegmentsService(tokens...)
+	if err != nil {
+		return nil, err
+	}
+	s, err := service.Get(segmentID).Do()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Segment{
+		ID:            segmentID,
+		Name:          s.Name,
+		Distance:      s.Distance,
+		ElevationHigh: s.ElevationHigh,
+		ElevationLow:  s.ElevationLow,
+	}, nil
+}
+
+func GetEfforts(segmentID int64) ([]*strava.SegmentEffortSummary, error) {
+	var efforts []*strava.SegmentEffortSummary
+
+	service, err := getSegmentsService(tokens...)
+	if err != nil {
+		return nil, err
+	}
+
+
+	for page := 1; page <= MAX_PAGES; page++ {
+		es, err := service.ListEfforts(segmentId).
+			PerPage(MAX_PER_PAGE).
+			Page(page).
+			Do()
+		if err != nil {
+			return nil, err
+		}
+		if len(efforts) == 0 {
+			break
+		}
+		efforts = append(efforts, es...)
+	}
+
+	return efforts, nil
+}
+
+func resource(filename string) string {
+	_, src, _, _ := runtime.Caller(0)
+	return filepath.Join(filepath.Dir(src), filename)
+}
+
+func getSegmentService(tokens ...string) (*strava.SegmentService, error) {
+	token = os.Getenv("STRAVA_ACCESS_TOKEN")
+	if len(tokens) > 0 && token[0] != "" {
+		token = tokens[0]
+	}
+	if token == "" {
+		return nil, fmt.Errorf("must provide a Strava access token")
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	client := strava.NewClient(token)
+	return strava.NewSegmentsService(client), nil
+}
